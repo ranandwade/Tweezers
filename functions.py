0cbd2faf019b088f67069d9fd0190a47601d224d
ritika <ritika5@uw.edu>
2023-08-04 14:41:29 -0700

    removing old things

--- functions.py
+++ functions.py
@@ -2,8 +2,6 @@ def potential(omegatweezer,linewidths,omegares,Popt,beamwaists):
     '''Find the potential of the optical tweezers beam for the given set of parameters -- without RWA
     omegatweezer = angular frequency of tweezer laser beam
     linewidths = linewidth of the given resonant transition (ex, S1/2 to D5/2)
-    Detuning = omegatweezer - omegaresonance where omegaresonance is the angular frequency corresponding to linewidth
-    counterrotating = omegatweezer+omegaresonance
     Popt = total optical power of tweezer laser beam
     beamwaists = beamwaist of the tweezer laser beam given its frequency and the NA of our system'''
     return (-3*pi*(c**2)/omegatweezer**3) * (linewidths/((omegares - omegatweezer)) +
@@ -12,7 +10,6 @@ def potentialRWA(omegatweezer,linewidths,omegares,Popt,beamwaists):
     '''Find the potential of the optical tweezers beam for the given set of parameters -- Using RWA
     omegatweezer = angular frequency of tweezer laser beam
     linewidths = linewidth of the given resonant transition (ex, S1/2 to D5/2)
-    Detuning = omegatweezer - omegaresonance where omegaresonance is the angular frequency corresponding to linewidth
     Popt = total optical power of tweezer laser beam
     beamwaists = beamwaist of the tweezer laser beam given its frequency and the NA of our system'''
     return (3*c**2/omegatweezer**3) * (linewidths/(omegares - omegatweezer)) * Popt/(beamwaists**2)
@@ -23,13 +20,12 @@ def scattering(omegatweezer,linewidths,omegares,Popt,beamwaists):
     for the given set of parameters -- without RWA
     omegatweezer = angular frequency of tweezer laser beam
     linewidths = linewidth of the given resonant transition (ex, S1/2 to D5/2)
-    Detuning = omegatweezer - omegaresonance where omegaresonance is the angular frequency corresponding to linewidth
     counterrotating = omegatweezer+omegaresonance
     Popt = total optical power of tweezer laser beam
     beamwaists = beamwaist of the tweezer laser beam given its frequency and the NA of our system'''
     return ((3*pi*(c**2))/(hbar * (omegatweezer**3))) *((omegatweezer/omegares)**3)* (((linewidths/(omegares - omegatweezer))+
                                                                             (linewidths/(omegares + omegatweezer)))**2) * ((Popt)/(beamwaists**2))
-def ep(d,wx,m):
+def epsilon(d,wx,m):
     """Dimensionless parameter describing characteristic energy(?) scales of our ion chain"""
     return ((e**2)/(4*pi*eps0*(d**3)*m*(wx**2)))**(1/2)
 
@@ -37,8 +33,8 @@ def v(wx,wxt):
     """Dimensionless ratio of radial trapping frequency of tweezer /rf trap"""
     return wxt/wx
 
-def wxt(U,beamwaists,m):
+def omega_radial(U,beamwaists,m):
     return ((abs(U) * 4) / (m * (beamwaists)**2))**(1/2)
 
-def wzt(U,beamwaists,tweezerwavelength,m):
+def omega_axial(U,beamwaists,tweezerwavelength,m):
     return ((2*abs(U)/m)**(1/2)) * 1/((pi*(beamwaist**2)/tweezerwavelength))
