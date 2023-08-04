1f5b9eb891ef534ffcd3034fcddd5f604a6703b4
ritika <ritika5@uw.edu>
2023-08-04 14:50:00 -0700

    Making Mindy's style edits

--- functions.py
+++ functions.py
@@ -1,30 +1,29 @@
-def potential(omega_tweezer,linewidths,omega_res,P_opt,beamwaists):
+def potential(omega_tweezer,linewidths,omega_res,P_opt,beam_waists):
     '''Find the potential of the optical tweezers beam for the given set of parameters -- without RWA
-    omegatweezer = angular frequency of tweezer laser beam
+    omega_tweezer = angular frequency of tweezer laser beam
     linewidths = linewidth of the given resonant transition (ex, S1/2 to D5/2)
-    Popt = total optical power of tweezer laser beam
-    beamwaists = beamwaist of the tweezer laser beam given its frequency and the NA of our system'''
+    P_opt = total optical power of tweezer laser beam
+    beam_waists = beamwaist of the tweezer laser beam given its frequency and the NA of our system'''
     return (-3*pi*(c**2)/omega_tweezer**3) * (linewidths/((omega_res - omega_tweezer)) +
-                                          linewidths/(omega_res + omega_tweezer)) * ((Popt)/(beamwaists**2))
-def potentialRWA(omega_tweezer,linewidths,omega_res,P_opt,beamwaists):
+                                          linewidths/(omega_res + omega_tweezer)) * ((P_opt)/(beam_waists**2))
+def potentialRWA(omega_tweezer,linewidths,omega_res,P_opt,beam_waists):
     '''Find the potential of the optical tweezers beam for the given set of parameters -- Using RWA
-    omegatweezer = angular frequency of tweezer laser beam
+    omega_tweezer = angular frequency of tweezer laser beam
     linewidths = linewidth of the given resonant transition (ex, S1/2 to D5/2)
-    Popt = total optical power of tweezer laser beam
-    beamwaists = beamwaist of the tweezer laser beam given its frequency and the NA of our system'''
-    return (3*c**2/omega_tweezer**3) * (linewidths/(omega_res - omega_tweezer)) * Popt/(beamwaists**2)
+    P_opt = total optical power of tweezer laser beam
+    beam_waists = beamwaist of the tweezer laser beam given its frequency and the NA of our system'''
+    return (3*c**2/omega_tweezer**3) * (linewidths/(omega_res - omega_tweezer)) * P_opt/(beam_waists**2)
 
 
 def scattering(omega_tweezer,linewidths,omega_res,P_opt,beamwaists):
     '''Find the scattering of the optical tweezers beam off of a given resonance
     for the given set of parameters -- without RWA
-    omegatweezer = angular frequency of tweezer laser beam
+    omega_tweezer = angular frequency of tweezer laser beam
     linewidths = linewidth of the given resonant transition (ex, S1/2 to D5/2)
-    counterrotating = omegatweezer+omegaresonance
-    Popt = total optical power of tweezer laser beam
-    beamwaists = beamwaist of the tweezer laser beam given its frequency and the NA of our system'''
+    P_opt = total optical power of tweezer laser beam
+    beam_waists = beamwaist of the tweezer laser beam given its frequency and the NA of our system'''
     return ((3*pi*(c**2))/(hbar * (omega_tweezer**3))) *((omega_tweezer/omegares)**3)* (((linewidths/(omega_res - omega_tweezer))+
-                                                                            (linewidths/(omegares + omega_tweezer)))**2) * ((Popt)/(beamwaists**2))
+                                                                            (linewidths/(omegares + omega_tweezer)))**2) * ((P_opt)/(beam_waists**2))
 def epsilon(d,wx,m):
     """Dimensionless parameter describing characteristic energy(?) scales of our ion chain"""
     return ((e**2)/(4*pi*eps0*(d**3)*m*(wx**2)))**(1/2)
@@ -33,8 +32,8 @@ def v(wx,wxt):
     """Dimensionless ratio of radial trapping frequency of tweezer /rf trap"""
     return wxt/wx
 
-def omega_radial(U,beamwaists,m):
-    return ((abs(U) * 4) / (m * (beamwaists)**2))**(1/2)
+def omega_radial(U,beam_waists,m):
+    return ((abs(U) * 4) / (m * (beam_waists)**2))**(1/2)
 
-def omega_axial(U,beamwaists,tweezerwavelength,m):
-    return ((2*abs(U)/m)**(1/2)) * 1/((pi*(beamwaist**2)/tweezer_wavelength))
+def omega_axial(U,beam_waists,tweezer_wavelength,m):
+    return ((2*abs(U)/m)**(1/2)) * 1/((pi*(beam_waist**2)/tweezer_wavelength))
